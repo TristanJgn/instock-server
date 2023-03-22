ab2ed1971585f7236d5fcae9241e7dbfd0aaa228
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-21 23:02:18 -0400

    adds controller export to handle the get request by making the appropriate query to the SQL database

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,3 +1,25 @@
+const knex = require("knex")(require("../knexfile"));
+
 exports.index = (_req, res) => {
   res.send("Warehouse homepage");
+};
+
+exports.singleWarehouse = (req, res) => {
+  knex("warehouses")
+    .where({ id: req.params.id })
+    .then((warehouses) => {
+      if (warehouses.length === 0) {
+        return res.status(404).json({
+          message: `Unable to find warehouse with id: ${req.params.id}`,
+        });
+      }
+
+      res.json(warehouses[0]);
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        message: "There was an issue with the request",
+        error,
+      });
+    });
 };
