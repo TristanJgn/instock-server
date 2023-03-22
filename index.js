091b1c08f2697e64a184e279356044b63e55201a
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-21 21:53:37 -0400

    adds basic API routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const warehouseRoutes = require("./routes/warehouseRoute");
+const inventoryRoutes = require("./routes/inventoryRoute");
 
 // Create an instance of express
 const app = express();
@@ -12,6 +14,10 @@ const PORT = process.env.PORT || 8000;
 // Add middleware
 app.use(cors());
 app.use(express.json());
+
+// Define routes
+app.use("/warehouses", warehouseRoutes);
+app.use("/inventory", inventoryRoutes);
 
 // Bootstrap server
 app.listen(PORT, () => {
