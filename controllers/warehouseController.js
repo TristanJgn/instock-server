ac3797ac2a826a561a7b299866b3e15685a4c3fd
jcamara <camarajoao1@gmail.com>
2023-03-22 14:15:09 -0700

    add route to warehouse list aka homepage

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,7 +1,25 @@
 const knex = require("knex")(require("../knexfile"));
 
-exports.index = (_req, res) => {
-  res.send("Warehouse homepage");
+exports.index = (req, res) => {
+  knex("warehouses")
+    .select(
+      "warehouse_name",
+      "address",
+      "city",
+      "country",
+      "contact_name",
+      "contact_phone",
+      "contact_email",
+    )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "There was an issue with the request",
+        err,
+      });
+    });
 };
 
 exports.singleWarehouse = (req, res) => {
