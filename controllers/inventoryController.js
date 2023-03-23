97d118f903390671ff5d88b543890cc519af1a85
hejceder <42016520+hejceder@users.noreply.github.com>
2023-03-23 13:22:48 -0700

    added logic to inventoryController for back end get api request, also attempted to structure data object

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,3 +1,77 @@
-exports.index = (_req, res) => {
-  res.send("Inventory homepage");
+const knex = require("knex")(require("../knexfile"));
+
+exports.index = (req, res) => {
+  knex("inventories")
+    .join (
+      "warehouses",
+      "warehouse_id",
+      "=",
+      "warehouses.id"
+    )
+    .select(
+      "inventories.id",
+      "warehouses.warehouse_name",
+      "inventories.item_name",
+      "inventories.description",
+      "inventories.category",
+      "inventories.status",
+      "inventories.quantity",
+    )
+    .then((data) => {
+      const formattedData = data.map((item) => {
+        return {
+          id: item.id,
+          warehouse_name: item.warehouse_name,
+          description: item.description,
+          category: item.category,
+          status: item.status,
+          quantity: item.quantity,
+        };
+      });
+
+      res.json(formattedData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "Issue with request",
+        err,
+      });
+    });
 };
+
+
+
+
+// const knex = require("knex")(require("../knexfile"));
+
+// exports.index = (req, res) => {
+//   knex("inventories")
+//     .join (
+//       "warehouses",
+//       "warehouse_id",
+//       "=",
+//       "warehouses.id"
+//     )
+//     .select(
+//       "inventories.id",
+//       "warehouses.warehouse_name",
+//       "inventories.item_name",
+//       "inventories.description",
+//       "inventories.category",
+//       "inventories.status",
+//       "inventories.quantity",
+//     )
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json({
+//         message: "Issue with request",
+//         err,
+//       });
+//     });
+// };
+
+
