e887e8b29173a5b54f2fcf74e49d275b90d2af15
hejceder <42016520+hejceder@users.noreply.github.com>
2023-03-23 17:37:01 -0700

    excluded formatted data and ds store

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -18,18 +18,7 @@ exports.index = (req, res) => {
       "inventories.quantity",
     )
     .then((data) => {
-      const formattedData = data.map((item) => {
-        return {
-          id: item.id,
-          warehouse_name: item.warehouse_name,
-          description: item.description,
-          category: item.category,
-          status: item.status,
-          quantity: item.quantity,
-        };
-      });
-
-      res.json(formattedData);
+      res.json(data);
     })
     .catch((err) => {
       console.log(err);
@@ -43,35 +32,5 @@ exports.index = (req, res) => {
 
 
 
-// const knex = require("knex")(require("../knexfile"));
-
-// exports.index = (req, res) => {
-//   knex("inventories")
-//     .join (
-//       "warehouses",
-//       "warehouse_id",
-//       "=",
-//       "warehouses.id"
-//     )
-//     .select(
-//       "inventories.id",
-//       "warehouses.warehouse_name",
-//       "inventories.item_name",
-//       "inventories.description",
-//       "inventories.category",
-//       "inventories.status",
-//       "inventories.quantity",
-//     )
-//     .then((data) => {
-//       res.json(data);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json({
-//         message: "Issue with request",
-//         err,
-//       });
-//     });
-// };
 
 
